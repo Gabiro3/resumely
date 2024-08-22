f04d50963860459597d5df0cc31bdb30316594f9
gabiroarnaud <gabiroarnaud@gmail.com>
2024-08-22 09:19:33 +0000

    Initial commit

---
+++ src/auth/sign-up/index.jsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/clerk-react'
+import React from 'react'
+
+function SignInPage() {
+  return (
+    <div className='flex justify-center my-20 items-center'>
+      <SignUp />
+    </div>
+  )
+}
+
+export default SignInPage
